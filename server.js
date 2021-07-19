1a889bedfe107f0d52109822268654ab3f8c3ea3
Pedro Martins <pmdmartins@gmail.com>
2021-07-19 23:12:22 +0100

    final

---
+++ server.js
@@ -0,0 +1,54 @@
+const express = require('express')
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex');
+
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
+const db = knex({
+    client: 'pg',
+    connection: {
+      host : '127.0.0.1',
+      user : 'postgres',
+      password : 'ROOT',
+      database : 'smart-brain'
+    }
+  });
+
+
+const app = express();
+
+
+app.use(express.json());
+app.use(cors())
+
+
+app.get('/', (req, res) => {
+    res.send('success');
+})
+
+//sign in
+
+app.post('/signin',(req, res) => { signin.handleSignin(req, res, db, bcrypt)})
+
+//register
+
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
+
+//get user id
+
+app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req ,res, db) })
+
+//increase imagecount
+
+app.put('/image', (req, res) => { image.handleImage(req, res, db) })
+app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) })
+
+
+app.listen(3000 , () => {
+    console.log('app is running in 3000');
+} )
+
